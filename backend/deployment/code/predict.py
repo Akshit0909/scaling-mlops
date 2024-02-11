8d4b5c688ad17ef375d01b9efb950776fd41ec50
Ubuntu <ubuntu@ip-172-31-3-209.us-east-2.compute.internal>
2024-02-11 02:00:08 +0000

    adding backend from aws ec2 linux

---
+++ backend/deployment/code/predict.py
@@ -0,0 +1,42 @@
+import pickle
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+
+
+model_file = 'model_C=1.0.bin'
+
+with open(model_file, 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+app = Flask('churn')
+CORS(app, resources={r"/predict": {"origins": "*"}})
+
+@app.route('/', methods=['OPTIONS'])
+def handle_options():
+    response = jsonify({'message': 'CORS preflight request handled'})
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
+
+    result = {
+        'churn_probability': float(y_pred),
+        'churn': bool(churn)
+    }
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
